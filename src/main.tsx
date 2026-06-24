01bf415c843a3a3730bdf460005c654a5ab5f5f8
Yousif <yousifg028@gmail.com>
2026-06-24 18:55:32 +0300

    debug: add global error handler to show JS errors on white screen

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
+window.addEventListener('error', (e) => {
+  document.getElementById('root')!.innerHTML = '<pre style="padding:20px;white-space:pre-wrap;color:red;background:#fff;font-size:14">' + e.message + '\n' + e.filename + ':' + e.lineno + '\n' + (e.error?.stack || '') + '</pre>';
+});
+window.addEventListener('unhandledrejection', (e) => {
+  document.getElementById('root')!.innerHTML = '<pre style="padding:20px;white-space:pre-wrap;color:red;background:#fff;font-size:14">Unhandled: ' + e.reason + '</pre>';
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
